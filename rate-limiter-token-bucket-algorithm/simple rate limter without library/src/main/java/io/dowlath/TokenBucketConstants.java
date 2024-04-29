65992ebf1c07c61b76c4001aaecad0d4e92af907
DowlathBashaG <9671419+DowlathBashaG@users.noreply.github.com>
2024-04-30 00:25:58 +0530

    Add files via upload
    initial commit

---
+++ rate-limiter-token-bucket-algorithm/simple rate limter without library/src/main/java/io/dowlath/TokenBucketConstants.java
@@ -0,0 +1,9 @@
+package io.dowlath;
+
+public class TokenBucketConstants {
+    public static int numberOfRequest =  10;
+
+    public static int windowSizeForRateLimitInMilliSeconds = 1 * 1000;
+
+    public static int maxBucketSize = 10;
+}
